31c5c5888e30edd9a9e3b4e7759ba8dc3dd713c9
Lubna Intezar <lubna.khan2609@gmail.com>
2023-12-26 02:47:07 +0000

    Add new file

---
+++ patterns.py
@@ -0,0 +1,11 @@
+
+#initialize number of rows
+num_rows=5
+
+#loop for increament pattern
+for i in range(1,num_rows+1):
+    print("*" * i )
+
+ #loop for decrement pattern    
+for i in range(num_rows-1,0,-1):
+    print("*" * i)
